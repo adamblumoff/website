80171986e2a5b5f319d459b8a105d3f7e9be3c77
Adam Blumoff <ablumoff25@amherst.edu>
2025-01-09 23:36:33 -0600

    content add

--- src/About/subheadingData.js
+++ src/About/subheadingData.js
@@ -1,11 +1,31 @@
 const subheadingData = {
   1: [
     {
-      title: "About Me",
+      title: "Techy Stuff",
       content: (
         <>
           <p>
-            Wassup
+            Hello, person? If you're reading this, you're pretty cool and already know of me. However, I will happily reintroduce myself again. Hi, my name is Adam Blumoff, and I am a student at Amherst College studying Computer Science (before June 2025).
+            I am (probably) currently a senior in my final semester and looking to do something productive in tech.
+            I have been passionate about solving problems through technology since my first coding class in my junior year of high school (shoutout to Mr. Wu, AKA the goat, AKA my offensive line coach).
+            Although this stuff sometimes wants to make me throw my chair across the room, I always seem to find myself back in the chair and aggressively reading StackOverflow comments.
+            Whether making a video game in Unity, crafting some Othello engines for a class project, or making this website because I'm bored on winter break, these things negatively impact the daily recommended amount of Vitamin D I am supposed to get.
+          </p>
+        </>
+      ),
+    },
+    {
+      title: "Not So Techy Stuff",
+      content: (
+        <>
+          <p>
+            Anyway, even though I prefer to click-clack on my keyboard doing God knows what, I still like to do other things.
+            I have always played sports at a high level, including basketball (varsity), football (college), baseball (varsity), and golf (10ish handicap).
+            Also, I go to the gym everyday, which is the only thing that keeps me sane.
+            I also like to do the usual things like hanging out with friends, smoking a cigar on the golf course, and taking people's money in poker.
+            Overall, I like to think of myself as an ambitious, hardworking, and caring person who is always looking to learn and grow.
+            The funny thing is, it doesn't matter what I think; that's not the point of this website.
+            Only your opinion matters, good or bad, I hope you had some fun along the way.
           </p>
         </>
       ),
@@ -17,7 +37,23 @@ const subheadingData = {
       content: (
         <>
           <p>
-            Wassup
+            Graduating May 2025 with a Bachelor's in Computer Science from Amherst College.
+          </p>
+        </>
+      ),
+    },
+  ],
+  3: [
+    {
+      title: "Eye Thrive Internship Summer 2023",
+      content: (
+        <>
+          <p>
+            Implemented and oversaw the central data transfer system from paper to electronic records.
+            Doubled efficiency in gathering and sorting inventory/demographic statistics.
+            Researched and spearheaded other efficient data collection processes.
+            Observed and acted as a technical liaison in creating a wireframe. 
+
           </p>
         </>
       ),
